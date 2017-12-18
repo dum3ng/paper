a3e1eaf2518587fd05948f21b3cea7e35b192c83
dumeng <dumeng81@outlook.com>
2017-12-19 02:30:20 +0800

    headache menu icon

--- static/js/index.js
+++ static/js/index.js
@@ -1,31 +1,57 @@
-(function(){
-let $ = document.querySelector.bind(document);
-let recentsTab = $("#recents-tab");
-let updatesTab = $("#updates-tab");
-let tab0 = $("#recents");
-let tab1 = $("#updates");
+(function() {
 
-let changeTab = (e) => {
-    switch(e.target){
-        case recentsTab: {
-            recentsTab.classList.add('active');
-            updatesTab.classList.remove('active');
+    let drawer = document.querySelector(".drawer");
+    let navArea = document.querySelector('.nav-area');
+    let toggle = document.querySelector('.toggle-menu');
+    let menu = document.querySelector('.menu-icon');
+    const closeDrawer = () => {
+      // drawer.open = false;
+      drawer.classList.remove('show');
+      drawer.classList.add('hide');
+      menu.classList.remove('close-menu');
+      navArea.style.pointerEvents = "none";
+    };
+    const openDrawer = () => {
+      // drawer.open = true;
+      drawer.classList.add('show');
+      drawer.classList.remove('hide');
+      menu.classList.add('close-menu');
+      navArea.style.pointerEvents = "all";
+    };
+    const toggleDrawer = () => {
+      if (drawer.classList.contains("show")) {
+        closeDrawer();
+      } else {
+        openDrawer();
+      }
+    };
 
-            tab0.style.display = "block";
-            tab1.style.display = "none";
-            break;
+    toggle.addEventListener('click', () => {
+      toggleDrawer();
+    });
+
+    navArea.addEventListener('click', (e) => {
+      if (e.target === navArea) {
+        e.preventDefault();
+        if (drawer.classList.contains("show")) {
+          closeDrawer();
         }
-        case updatesTab: {
-            recentsTab.classList.remove('active');
-            updatesTab.classList.add('active');
+      }
+    });
 
-            tab1.style.display = "block";
-            tab0.style.display = "none";
-            break;
-        }
+    let dismiss = document.querySelector('.dismiss');
+    if (dismiss) {
+      let article = document.querySelector('.dismissable');
+      let uuid = dismiss.dataset["uuid"];
+      let hasExist = window.localStorage.getItem(uuid);
+      if (hasExist) {
+        article.style.display = "none";
+      }
+      dismiss.onclick = (e) => {
+        window.localStorage.setItem(uuid, true);
+        article.style.display = "none";
+      };
+
     }
-}
-recentsTab.onclick = changeTab;
-updatesTab.onclick = changeTab;
 
-})();
+  })();
